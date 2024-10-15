81fa6aebf64b649b66f4c9619c69235cd82e704e
Yatik <yatiksihag01@gmail.com>
2024-10-15 09:18:42 +0530

    feat: Implement breaking news functionality.

---
+++ android/app/src/main/java/com/meproject/newsstream/data/remote/dto/breaking/BreakingNewsDto.kt
@@ -0,0 +1,10 @@
+package com.meproject.newsstream.data.remote.dto.breaking
+
+import com.google.gson.annotations.SerializedName
+
+data class BreakingNewsDto (
+    val title: String,
+    val url: String,
+    @SerializedName("url_to_image")
+    val urlToImage: String
+)
