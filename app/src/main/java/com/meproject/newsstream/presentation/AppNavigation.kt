c4af544b678cbfccf4f5dfd39f91c4d9a8de22b8
Yatik <yatiksihag01@gmail.com>
2025-01-17 00:22:25 +0530

    Refactor: Implement token-based authentication using Flow.

--- app/src/main/java/com/meproject/newsstream/presentation/AppNavigation.kt
+++ app/src/main/java/com/meproject/newsstream/presentation/AppNavigation.kt
@@ -2,6 +2,7 @@ package com.meproject.newsstream.presentation
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
@@ -19,10 +20,12 @@ import kotlinx.serialization.Serializable
 fun NewsStreamApp() {
     NewsStreamTheme {
         val authViewModel: AuthViewModel = hiltViewModel()
-        val isLoggedIn = authViewModel.isLoggedIn.value
+        val accessToken = authViewModel.tokenFlow.collectAsStateWithLifecycle(null)
         val navController = rememberNavController()
-        val startDestination: NavigationDestination = if (isLoggedIn == true) Main else AuthGraph
-        NavHost(navController = navController, startDestination = startDestination) {
+        NavHost(
+            navController = navController,
+            startDestination = if (accessToken.value != null) Main else AuthGraph
+        ) {
             navigation<AuthGraph>(startDestination = Login) {
                 loginScreenDestination(
                     onNavigationToHome = { navController.navigate(Main) },
